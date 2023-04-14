4e4732efe958c0758d0989a2444e75bc26f6207d
FlorinBodogan <florin_bodogan@yahoo.com>
2023-04-14 17:53:09 +0300

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutmeComponent } from './components/aboutme/aboutme.component';
@@ -9,17 +8,16 @@ import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'header', component: HeaderComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutmeComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: '/header', component: HeaderComponent },
+  { path: '/home', component: HomeComponent },
+  { path: '/about', component: AboutmeComponent },
+  { path: '/projects', component: ProjectsComponent },
+  { path: '/contact', component: ContactComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}]
 })
 
 export class AppRoutingModule { }
